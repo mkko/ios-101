4ff4e54c9f1f7a6d545db012c64a9834db9667f1
Mikko Välimäki <mkko1373@gmail.com>
2019-05-22 21:11:13 +0300

    Initial commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Mikko Välimäki on 2019-05-22.
+//  Copyright © 2019 Mikko Välimäki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
